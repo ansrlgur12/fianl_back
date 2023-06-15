2cd951b5668d11d8b32b20fab832b68230beafcd
ansrlgur12 <ansrlgur12@naver.com>
2023-06-15 10:05:04 +0900

    백엔드 구성

---
+++ finalCamo/src/main/java/com/example/demo/FinalCamoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCamoApplication.class, args);
+	}
+
+}
