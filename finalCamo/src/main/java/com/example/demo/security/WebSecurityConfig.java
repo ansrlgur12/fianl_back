e93fb018993c87ddcaf13629142c63b51b686865
ansrlgur12 <ansrlgur12@naver.com>
2023-06-21 19:22:31 +0900

    캠핑데이터 조회

--- finalCamo/src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ finalCamo/src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/thymeleaf/**").permitAll()
-                .antMatchers("/camping-data").permitAll()
+                .antMatchers("/camping-data", "/campData").permitAll()
                 .antMatchers(HttpMethod.POST, "/json", "/camp-json").permitAll()
                 .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/sign-api/exception").permitAll()
                 .anyRequest().authenticated()
