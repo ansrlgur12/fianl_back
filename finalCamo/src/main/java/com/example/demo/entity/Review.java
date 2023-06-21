313d7e88691598e79c27220891692eae02172276
ansrlgur12 <ansrlgur12@naver.com>
2023-06-21 22:30:12 +0900

    오류해결

--- finalCamo/src/main/java/com/example/demo/entity/Review.java
+++ finalCamo/src/main/java/com/example/demo/entity/Review.java
@@ -1,38 +1,38 @@
-//package com.example.demo.entity;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//import javax.persistence.*;
-//import java.time.LocalDate;
-//
-///**
-// * 리뷰
-// */
-//@Entity
-//@Getter
-//@Setter
-//@ToString
-//@Table(name = "review")
-//public class Review {
-//    @Id
-//    @Column(name="product_id")
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id")
-//    private Member member;
-//
-//    @Column(nullable = false) //게시글 제목
-//    private String title;
-//
-//    @Column(nullable = false) //게시글 내용
-//    private String content;
-//
-//    @Column(nullable = false) //작성일자
-//    private LocalDate date;
-//
-//    @Column(nullable = false) //글타입
-//    private int postType;
-//}
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+/**
+ * 리뷰
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "review")
+public class Review {
+    @Id
+    @Column(name="product_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false) //게시글 제목
+    private String title;
+
+    @Column(nullable = false) //게시글 내용
+    private String content;
+
+    @Column(nullable = false) //작성일자
+    private LocalDate date;
+
+    @Column(nullable = false) //글타입
+    private int postType;
+}
