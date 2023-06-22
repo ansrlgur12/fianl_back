b20707ab85395f15ad35a5dbbca95ca26d918634
jshblack <118979741+JSH1990@users.noreply.github.com>
2023-06-22 12:42:56 +0900

    에러수정

--- finalCamo/src/main/java/com/example/demo/repository/OrdersRepository.java
+++ finalCamo/src/main/java/com/example/demo/repository/OrdersRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Camp;
+import com.example.demo.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CampRepository extends JpaRepository<Camp, Long> {
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
 }
