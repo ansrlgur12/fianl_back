ef31f7181745d4b462e6b88970f964144036af16
jshblack <118979741+JSH1990@users.noreply.github.com>
2023-06-25 21:36:48 +0900

    ã리뷰게ã시판, 좋아요 수

--- finalCamo/src/main/java/com/example/demo/repository/LikesRepository.java
+++ finalCamo/src/main/java/com/example/demo/repository/LikesRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Camp;
 import com.example.demo.entity.Likes;
 import com.example.demo.entity.Member1;
 import com.example.demo.entity.Product;
@@ -8,8 +9,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-    int countByProduct(Product product); //좋아요 갯수 증가
-    void deleteByMember1AndProduct(Member1 member1, Product product); //좋아요 삭제
-}
+    int countByProduct(Product product);
+    void deleteByMember1AndProduct(Member1 member1, Product product);
 
-//    Likes findByMember1AndProduct(Member1 member1, Product product); //특정 회원 좋아요 조회
+    int countByCamp(Camp camp);
+    void deleteByMember1AndCamp(Member1 member1, Camp camp);
+}
