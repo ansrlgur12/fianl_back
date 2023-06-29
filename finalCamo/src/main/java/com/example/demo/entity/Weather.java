42b081268f2978a1b4c85fdf1583fd4a3f25b42e
ansrlgur12 <ansrlgur12@naver.com>
2023-06-30 02:04:42 +0900

    날씨

--- finalCamo/src/main/java/com/example/demo/entity/Weather.java
+++ finalCamo/src/main/java/com/example/demo/entity/Weather.java
@@ -4,11 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @ToString
 public class Weather {
+    @Id
+    @Column(name = "weather_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fcstDate; // 예보날짜
+    private String fcstTime; // 예보시간
+    private String category; // 예보유형
+    private String fcstValue; // 예보값
+    private String nx; // x좌표값
+    private String ny; // y좌표값
+
 }
