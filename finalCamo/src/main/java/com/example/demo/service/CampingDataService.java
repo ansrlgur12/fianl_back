d5bafa3a2cbf60c6d31c99a7f107a97efe0f9fe9
ansrlgur12 <ansrlgur12@naver.com>
2023-07-10 14:27:03 +0900

    메인페이지 하단

--- finalCamo/src/main/java/com/example/demo/service/CampingDataService.java
+++ finalCamo/src/main/java/com/example/demo/service/CampingDataService.java
@@ -122,6 +122,7 @@ public class CampingDataService {
         if("ALL".equals(dho) && "시.군.구".equals(sigungu)){
             for (Camp camp : items) {
                 CampDto campDto = new CampDto();
+                campDto.setId(camp.getId());
                 campDto.setAnimalCmgCl(camp.getAnimalCmgCl());
                 campDto.setFacltNm(camp.getFacltNm());
                 campDto.setAddr1(camp.getAddr1());
@@ -137,6 +138,7 @@ public class CampingDataService {
         } else if (!"ALL".equals(dho) && "시.군.구".equals(sigungu)) {
             for (Camp camp : itemsByDho) {
                 CampDto campDto = new CampDto();
+                campDto.setId(camp.getId());
                 campDto.setAnimalCmgCl(camp.getAnimalCmgCl());
                 campDto.setFacltNm(camp.getFacltNm());
                 campDto.setAddr1(camp.getAddr1());
@@ -152,6 +154,7 @@ public class CampingDataService {
         } else {
             for (Camp camp : itemsBySelect) {
                 CampDto campDto = new CampDto();
+                campDto.setId(camp.getId());
                 campDto.setAnimalCmgCl(camp.getAnimalCmgCl());
                 campDto.setFacltNm(camp.getFacltNm());
                 campDto.setAddr1(camp.getAddr1());
